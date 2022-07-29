bb98e6e63d633eaeb4920d661e5d8a0e41f93199
zbysulak <sedlik31@gmail.com>
2022-07-29 15:36:17 +0200

    Initial commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Chef;
+
+Console.WriteLine("Hello, World!");
+
+var k = new Kitchen();
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+    if (i == 10)
+    {
+        k.PrepareFries();
+        //k.PrepareFood();
+    }
+    if (i == 11)
+    {
+        k.TakeFromFrier();
+    }
+    if (i == 30)
+    {
+        k.TakeFromFrier();
+    }
+    Thread.Sleep(100);
+}
